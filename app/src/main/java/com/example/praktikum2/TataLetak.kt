026ffb8aca71b6839ba21754e21da533d73e24f8
DhaniCuPliZ <ramadhanirafli065@gmail.com>
2025-10-16 18:01:17 +0700

    tambahkan Row kedua pada Box di fungsi TataletakBoxColumnRow dengan tiga komponen teks

--- app/src/main/java/com/example/praktikum2/TataLetak.kt
+++ app/src/main/java/com/example/praktikum2/TataLetak.kt
@@ -110,4 +110,15 @@ fun TataletakBoxColumnRow(modifier: Modifier) {
                     Text(text = "Col1_Row1_Kom2")
                     Text(text = "Col1_Row1_Kom3")
                 }
+                Row(
+                    modifier = modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    Text(text = "Col1_Row2_Komponen1")
+                    Text(text = "Col1_Row2_Komponen2")
+                    Text(text = "Col1_Row2_Komponen3")
+                }
+            }
         }
+
+    }
