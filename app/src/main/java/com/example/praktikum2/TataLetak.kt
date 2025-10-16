0da8ef893ec68d878a6c36951f9fb6a37bcff77a
DhaniCuPliZ <ramadhanirafli065@gmail.com>
2025-10-16 18:03:19 +0700

    tambahkan komponen Image pada Box kedua di fungsi TataletakBoxColumnRow

--- app/src/main/java/com/example/praktikum2/TataLetak.kt
+++ app/src/main/java/com/example/praktikum2/TataLetak.kt
@@ -128,6 +128,9 @@ fun TataletakBoxColumnRow(modifier: Modifier) {
                 .background(color = Color.Cyan),
             contentAlignment = Alignment.Center
         ){
-
+            Image(painter = gambar,
+                contentDescription = null,
+                contentScale = ContentScale.Fit)
+        }
 
         }
