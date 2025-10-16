3fe00e6f67c2390068224603ebcfbbc576e7f72e
DhaniCuPliZ <ramadhanirafli065@gmail.com>
2025-10-16 17:54:30 +0700

    tambahkan fungsi TataletakBox menggunakan komponen Box dengan elemen Text di tengan

--- app/src/main/java/com/example/praktikum2/TataLetak.kt
+++ app/src/main/java/com/example/praktikum2/TataLetak.kt
@@ -36,3 +36,19 @@ fun TataletakColumn(modifier: Modifier) {
     }
 }
 
+@Composable
+fun TataletakBox(modifier: Modifier) {
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .fillMaxWidth(), contentAlignment = Alignment.Center
+    ) {
+        Text(text = "Box 1")
+        Text(text = "Column 1")
+        Text(text = "Row 1")
+        Text(text = "Box 2")
+        Text(text = "Column 2")
+    }
+}
+
+
